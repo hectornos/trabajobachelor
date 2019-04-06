23a189679efa9953c79378f0dfb25e988232a573
hectornos <hectornos@gmail.com>
2019-04-06 17:31:45 +0200

    otro comit

---
+++ funciones/tema1.js
@@ -0,0 +1,9 @@
+function adios(){
+
+  alert("Adiosito");
+}
+
+function dias(){
+  var dias = ['lunes','martes','miercoles','jueves','viernes','sabado','domingo'];
+  alert('Dias de la semana:\n' + dias.join('\n'));
+}
